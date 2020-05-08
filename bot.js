143b25d38a5a148356bea6685dce31a2f64a6532
Hauk Olaussen <olaussen@users.noreply.github.com>
2020-05-09 00:34:56 +0200

    Moved commands to own module

--- bot.js
+++ bot.js
@@ -3,27 +3,33 @@ require('dotenv').config()
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+const commands = require('./commands').commands()
+
 const prefix = '!pp'
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
-const validateArgs = (msg, args) => {
-    if(args.length != 2) {
-        msg.reply("Slutt å vær så retarded a please!")
+const validateArgs = async (message, args) => {
+    const command = commands[args]
+    const channel = message.member.voice.channel
+    if (!command) {
+        message.reply("Slutt å vær så retarded a please!")
         return
-    } 
-    msg.reply("Du skrev inn kommandoen: " + args[1])
-} 
-
-client.on('message', msg => {
-    const content = msg.content
-    const args = content.split(" ")
-    const givenprefix = args[0]
-    if (givenprefix === prefix) {
-        validateArgs(msg, args)
     }
+    if (channel) {
+        command.run(channel, message)
+    } else {
+        message.channel.send('You need to join a voice channel first!');
+    }
+}
+
+client.on('message', message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+    const args = message.content.slice(prefix.length).split(' ');
+    args.shift().toLowerCase();
+    validateArgs(message, args)
 });
 
 client.login(process.env.BOT_TOKEN);
--- commands.js
+++ commands.js
@@ -1,6 +1,25 @@
 
 
-const commands = {
+const ytdl = require('ytdl-core');
 
+exports.commands = function () {
+    return {
+        play: {
+            run: async (channel, message, playlistname = "") => {
+                const connection = await channel.join();
+                if (playlistname == "") {
+                    message.channel.send("Her har du litt 'Ole er homo' good stuff!")
+                    connection.play(ytdl("https://www.youtube.com/watch?v=CuPTzTtQnCI&t=51s", { filter: 'audioonly' }));
+                } else {
 
+                }
+            }
+        },
+        kick: {
+            run: (channel, message) => {
+                message.channel.send("Imma 'bout to head out!")
+                channel.leave()
+            }
+        }
+    }
 }
